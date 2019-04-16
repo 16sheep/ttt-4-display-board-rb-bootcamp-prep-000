ae94c033f35436f931b7dc923bb5ef93f3216370
marjutubli <marjutubli@gmail.com>
2019-04-16 21:06:35 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -7,7 +7,7 @@ def display_board(board)
   puts "-----------"
   #puts " "+board[6] +"| "+board[7]+" |  "+board[8]
   
-  puts " #{board[0]} | #{board[7]} | #{board[8]} "
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
   
   
   
